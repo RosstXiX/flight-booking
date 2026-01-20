022e6a99b182fed47188f3bf9bf499b15bffbd25
Rostislav <rstk.tischenko@gmail.com>
2026-01-20 20:01:54 +0200

    fix flight search by departure airport timezone instead of UTC

--- src/main/java/io/github/rosstxix/flightbooking/service/FlightService.java
+++ src/main/java/io/github/rosstxix/flightbooking/service/FlightService.java
@@ -1,9 +1,11 @@
 package io.github.rosstxix.flightbooking.service;
 
+import io.github.rosstxix.flightbooking.domain.entity.Airport;
 import io.github.rosstxix.flightbooking.dto.response.FlightSearchResponse;
 import io.github.rosstxix.flightbooking.dto.request.FlightSearchRequest;
 import io.github.rosstxix.flightbooking.domain.entity.Flight;
 import io.github.rosstxix.flightbooking.mapper.FlightMapper;
+import io.github.rosstxix.flightbooking.repository.AirportRepository;
 import io.github.rosstxix.flightbooking.repository.FlightRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
@@ -17,17 +19,28 @@ import java.util.stream.Collectors;
 public class FlightService {
     private final FlightRepository flightRepository;
     private final FlightMapper flightMapper;
+    private final AirportRepository airportRepository;
 
-    public FlightService(FlightRepository flightRepository, FlightMapper flightMapper) {
+    public FlightService(
+            FlightRepository flightRepository,
+            FlightMapper flightMapper,
+            AirportRepository airportRepository
+    ) {
         this.flightRepository = flightRepository;
         this.flightMapper = flightMapper;
+        this.airportRepository = airportRepository;
     }
 
     public List<FlightSearchResponse> searchFlights(FlightSearchRequest request) {
         // Convert LocalDate to UTC diapason for searching
-        // TODO: Search by departure airport timezone, not UTC
-        Instant startUtc = request.date().atStartOfDay(ZoneId.of("UTC")).toInstant();
-        Instant endUtc = request.date().plusDays(1).atStartOfDay(ZoneId.of("UTC")).toInstant();
+
+        Airport departureAirport = airportRepository.findByCode(request.fromCode())
+                .orElseThrow(() -> new EntityNotFoundException("Airport with code=%s not found".formatted(request.fromCode())));
+
+        ZoneId zone = ZoneId.of(departureAirport.getTimeZone());
+
+        Instant startUtc = request.date().atStartOfDay(zone).toInstant();
+        Instant endUtc = request.date().plusDays(1).atStartOfDay(zone).toInstant();
 
         List<Flight> flights = flightRepository.searchFlights(
                 request.fromCode(),
