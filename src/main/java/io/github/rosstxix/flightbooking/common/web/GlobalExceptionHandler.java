1db50fa7974db31a8f2d54d4613827e4e95b190b
Rostislav <rstk.tischenko@gmail.com>
2026-01-24 08:38:13 +0200

    add exception handling for validation

--- src/main/java/io/github/rosstxix/flightbooking/common/web/GlobalExceptionHandler.java
+++ src/main/java/io/github/rosstxix/flightbooking/common/web/GlobalExceptionHandler.java
@@ -1,11 +1,17 @@
 package io.github.rosstxix.flightbooking.common.web;
 
+import io.github.rosstxix.flightbooking.common.error.ApiErrorCode;
 import io.github.rosstxix.flightbooking.common.exception.ApiException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import io.github.rosstxix.flightbooking.common.error.ErrorResponse;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -24,4 +30,26 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.status(ex.getHttpStatus()).body(response);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(
+            MethodArgumentNotValidException ex,
+            HttpServletRequest request
+    ) {
+        String message = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + " : " + error.getDefaultMessage())
+                .collect(Collectors.joining("; "));
+
+        ErrorResponse response = new ErrorResponse(
+                HttpStatus.BAD_REQUEST,
+                ApiErrorCode.VALIDATION_ERROR,
+                message,
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
 }
