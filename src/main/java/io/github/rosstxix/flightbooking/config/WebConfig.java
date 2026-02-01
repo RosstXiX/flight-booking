ca44477e1b2bdcb03e601b45f66b79044dee16b6
Rostislav <rstk.tischenko@gmail.com>
2026-02-01 04:23:43 +0200

    fix message format at CommonsRequestLoggingFilter

--- src/main/java/io/github/rosstxix/flightbooking/config/WebConfig.java
+++ src/main/java/io/github/rosstxix/flightbooking/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
         filter.setIncludePayload(false);    // Not logging request body
         filter.setMaxPayloadLength(10000);
         filter.setIncludeHeaders(false);
-        filter.setAfterMessagePrefix("REQUEST DATA: ");
+        filter.setAfterMessagePrefix("REQUEST DATA: [");
 
         return filter;
     }
