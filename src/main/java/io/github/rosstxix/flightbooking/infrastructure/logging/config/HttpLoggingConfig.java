5e3bc655fc7eabad8e24cd9eefc28132cda13b9f
Rostislav <rstk.tischenko@gmail.com>
2026-02-17 16:25:04 +0200

    chore: make requestLoggingFilter order explicit

--- src/main/java/io/github/rosstxix/flightbooking/infrastructure/logging/config/HttpLoggingConfig.java
+++ src/main/java/io/github/rosstxix/flightbooking/infrastructure/logging/config/HttpLoggingConfig.java
@@ -2,12 +2,15 @@ package io.github.rosstxix.flightbooking.infrastructure.logging.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @Configuration
 public class HttpLoggingConfig {
 
     @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE + 10)
     public CommonsRequestLoggingFilter requestLoggingFilter() {
         CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
 
