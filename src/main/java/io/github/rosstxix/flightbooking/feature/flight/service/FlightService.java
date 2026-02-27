000d37e830d01a41846b7c4e83b00e8262b416cc
Rostislav <rstk.tischenko@gmail.com>
2026-02-27 06:27:48 +0200

    refactor: remove unnecessary interface and simplify service name - remove the FlightService interface, as its abstraction was not used - rename the service implementation to FlightService

--- src/main/java/io/github/rosstxix/flightbooking/feature/flight/service/FlightService.java
+++ src/main/java/io/github/rosstxix/flightbooking/feature/flight/service/FlightService.java
@@ -1,15 +1,74 @@
 package io.github.rosstxix.flightbooking.feature.flight.service;
 
 import io.github.rosstxix.flightbooking.dto.PageResponse;
-import io.github.rosstxix.flightbooking.feature.flight.dto.request.FlightSearchRequest;
+import io.github.rosstxix.flightbooking.infrastructure.error.exception.EntityNotFoundApiException;
+import io.github.rosstxix.flightbooking.domain.Airport;
+import io.github.rosstxix.flightbooking.feature.flight.dto.projection.FlightProjection;
 import io.github.rosstxix.flightbooking.feature.flight.dto.response.FlightSearchResponse;
+import io.github.rosstxix.flightbooking.feature.flight.dto.request.FlightSearchRequest;
+import io.github.rosstxix.flightbooking.feature.flight.mapper.FlightMapper;
+import io.github.rosstxix.flightbooking.repository.AirportRepository;
+import io.github.rosstxix.flightbooking.feature.flight.repository.FlightRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface FlightService {
-    PageResponse<FlightSearchResponse> searchFlights(
-            FlightSearchRequest request,
-            Pageable pageable
-    );
+import java.time.Instant;
+import java.time.ZoneId;
 
-    FlightSearchResponse getFlightDetails(Long id);
+@Slf4j
+@Service
+public class FlightService {
+    private final FlightRepository flightRepository;
+    private final FlightMapper flightMapper;
+    private final AirportRepository airportRepository;
+
+    public FlightService(
+            FlightRepository flightRepository,
+            FlightMapper flightMapper,
+            AirportRepository airportRepository
+    ) {
+        this.flightRepository = flightRepository;
+        this.flightMapper = flightMapper;
+        this.airportRepository = airportRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public PageResponse<FlightSearchResponse> searchFlights(FlightSearchRequest request, Pageable pageable) {
+        // Convert LocalDate to UTC diapason for searching
+
+        Airport departureAirport = airportRepository.findByCode(request.fromCode())
+                .orElseThrow(() ->
+                    new EntityNotFoundApiException("Airport with code %s not found".formatted(request.fromCode()))
+                );
+
+        ZoneId zone = ZoneId.of(departureAirport.getTimeZone());
+
+        Instant startUtc = request.date().atStartOfDay(zone).toInstant();
+        Instant endUtc = request.date().plusDays(1).atStartOfDay(zone).toInstant();
+
+        Page<FlightProjection> projections = flightRepository.searchFlights(
+                request.fromCode(),
+                request.toCode(),
+                startUtc,
+                endUtc,
+                pageable
+        );
+
+        Page<FlightSearchResponse> response = projections.map(flightMapper::toSearchResponse);
+        return PageResponse.from(response);
+    }
+
+    @Transactional(readOnly = true)
+    public FlightSearchResponse getFlightDetails(Long id) {
+        FlightProjection projection = flightRepository.findProjectionById(id)
+                .orElseThrow(() ->
+                    new EntityNotFoundApiException("Flight with id %d not found".formatted(id))
+                );
+
+        return flightMapper.toSearchResponse(projection);
+    }
+
 }
