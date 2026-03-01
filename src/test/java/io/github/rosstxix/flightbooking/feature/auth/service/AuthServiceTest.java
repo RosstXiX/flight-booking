b00171338bc6b30db06c2b112ce0055deba688f1
Rostislav <rstk.tischenko@gmail.com>
2026-03-01 06:45:24 +0200

    test: add unit tests for AuthService and FlightService - add test coverage for AuthService methods: login and register - add test coverage for FlightService methods: searchFlights and getFlightDetails - disable FlightBookingApplicationTests class

---
+++ src/test/java/io/github/rosstxix/flightbooking/feature/auth/service/AuthServiceTest.java
@@ -0,0 +1,148 @@
+package io.github.rosstxix.flightbooking.feature.auth.service;
+
+import io.github.rosstxix.flightbooking.feature.auth.dto.request.LoginRequest;
+import io.github.rosstxix.flightbooking.feature.auth.dto.request.RegisterRequest;
+import io.github.rosstxix.flightbooking.feature.auth.dto.response.LoginResponse;
+import io.github.rosstxix.flightbooking.feature.user.domain.User;
+import io.github.rosstxix.flightbooking.feature.user.repository.UserRepository;
+import io.github.rosstxix.flightbooking.infrastructure.error.exception.EmailAlreadyExistsApiException;
+import io.github.rosstxix.flightbooking.infrastructure.security.jwt.JwtService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class AuthServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private AuthenticationManager authenticationManager;
+    @Mock
+    private JwtService jwtService;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @InjectMocks
+    private AuthService authService;
+
+    // -------------------
+    // login
+    // -------------------
+
+    @Test
+    void login_shouldReturnLoginResponse_whenCredentialsValid() {
+        // Arrange
+        LoginRequest request = new LoginRequest("user@example.com", "password1234");
+        UserDetails userDetails = mock(UserDetails.class);
+        Authentication authentication = mock(Authentication.class);
+
+        when(authentication.getPrincipal()).thenReturn(userDetails);
+        when(authenticationManager.authenticate(
+                argThat(token ->
+                        token instanceof UsernamePasswordAuthenticationToken t &&
+                        t.getPrincipal().equals(request.email()) &&
+                        t.getCredentials().equals(request.password())
+                )
+        )).thenReturn(authentication);
+
+        String expectedToken = "token";
+        long expectedExpiration = 3600L;
+
+        when(jwtService.generateToken(userDetails)).thenReturn(expectedToken);
+        when(jwtService.getExpirationSeconds()).thenReturn(expectedExpiration);
+
+        // Act
+        LoginResponse response = authService.login(request);
+
+        // Assert
+        assertThat(response.accessToken()).isEqualTo(expectedToken);
+        assertThat(response.tokenType()).isEqualTo("Bearer");
+        assertThat(response.expiresIn()).isEqualTo(expectedExpiration);
+
+    }
+
+    @Test
+    void login_shouldThrowBadCredentialsException_whenCredentialsInvalid() {
+        // Arrange
+        String expectedMessage = "Invalid credentials";
+        LoginRequest request = new LoginRequest("badEmail@example.com", "password1234");
+
+        when(authenticationManager.authenticate(
+                argThat(token ->
+                        token instanceof UsernamePasswordAuthenticationToken t &&
+                        t.getPrincipal().equals(request.email()) &&
+                        t.getCredentials().equals(request.password())
+                )
+        )).thenThrow(new BadCredentialsException(expectedMessage));
+
+        // Act + Assert
+        assertThatThrownBy(() -> authService.login(request))
+                .isInstanceOf(BadCredentialsException.class)
+                .hasMessage(expectedMessage);
+
+        verifyNoInteractions(jwtService);
+    }
+
+    // -------------------
+    // register
+    // -------------------
+
+    @Test
+    void register_shouldSaveUserWithEncodedPassword_whenEmailNotExists() {
+        // Arrange
+        RegisterRequest request = buildRegisterRequest();
+        String encodedPassword = "encodedPassword";
+
+        when(userRepository.existsByEmail(request.email())).thenReturn(false);
+        when(passwordEncoder.encode(request.password())).thenReturn(encodedPassword);
+
+        // Act
+        authService.register(request);
+
+        // Assert
+        ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
+        verify(userRepository).save(userCaptor.capture());
+
+        User savedUser = userCaptor.getValue();
+        assertThat(savedUser.getEmail()).isEqualTo(request.email());
+        assertThat(savedUser.getPassword()).isEqualTo(encodedPassword);
+        assertThat(savedUser.getFirstName()).isEqualTo(request.firstName());
+        assertThat(savedUser.getLastName()).isEqualTo(request.lastName());
+    }
+
+    @Test
+    void register_shouldThrowEmailAlreadyExistsApiException_whenEmailExists() {
+        // Arrange
+        RegisterRequest request = buildRegisterRequest();
+
+        when(userRepository.existsByEmail(request.email())).thenReturn(true);
+
+        // Act + Assert
+        assertThatThrownBy(() -> authService.register(request))
+                .isInstanceOf(EmailAlreadyExistsApiException.class)
+                .hasMessageContaining(request.email());
+
+        verify(userRepository, never()).save(any());
+    }
+
+    private static RegisterRequest buildRegisterRequest() {
+        return new RegisterRequest(
+                "user@example.com", "password1234", "John", "Doe"
+        );
+    }
+
+
+}
---
+++ src/test/java/io/github/rosstxix/flightbooking/feature/flight/service/FlightServiceTest.java
@@ -0,0 +1,291 @@
+package io.github.rosstxix.flightbooking.feature.flight.service;
+
+import io.github.rosstxix.flightbooking.common.dto.PageResponse;
+import io.github.rosstxix.flightbooking.feature.catalog.domain.Airport;
+import io.github.rosstxix.flightbooking.feature.catalog.repository.AirportRepository;
+import io.github.rosstxix.flightbooking.feature.flight.domain.FlightStatus;
+import io.github.rosstxix.flightbooking.feature.flight.dto.projection.FlightProjection;
+import io.github.rosstxix.flightbooking.feature.flight.dto.request.FlightSearchRequest;
+import io.github.rosstxix.flightbooking.feature.flight.dto.response.FlightSearchResponse;
+import io.github.rosstxix.flightbooking.feature.flight.mapper.FlightMapper;
+import io.github.rosstxix.flightbooking.feature.flight.repository.FlightRepository;
+import io.github.rosstxix.flightbooking.infrastructure.error.exception.EntityNotFoundApiException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+class FlightServiceTest {
+
+    private static final String FROM_CODE = "KBP";
+    private static final String TO_CODE = "LWO";
+    private static final LocalDate SEARCH_DATE = LocalDate.of(2026, 10, 20);
+
+    @Mock
+    private FlightRepository flightRepository;
+    @Mock
+    private FlightMapper flightMapper;
+    @Mock
+    private AirportRepository airportRepository;
+    @InjectMocks
+    private FlightService flightService;
+
+    // -------------------
+    // searchFlights
+    // -------------------
+
+    @Test
+    void searchFlights_shouldReturnPageResponse_whenFlightsFound() {
+        // Arrange
+        FlightSearchRequest request = buildSearchRequest();
+        Pageable pageable = PageRequest.of(0, 10);
+        Airport departureAirport = buildDepartureAirport();
+        FlightProjection projection = mock(FlightProjection.class);
+        long totalElements = 1L;
+        Page<FlightProjection> projections = new PageImpl<>(
+                List.of(projection),
+                pageable,
+                totalElements
+        );
+        FlightSearchResponse mappedResponse = buildFlightSearchResponse();
+
+        when(airportRepository.findByCode(FROM_CODE))
+                .thenReturn(Optional.of(departureAirport));
+
+        when(flightRepository.searchFlights(any(), any(), any(), any(), any(), any()))
+                .thenReturn(projections);
+
+        when(flightMapper.toSearchResponse(projection))
+                .thenReturn(mappedResponse);
+
+        // Act
+        PageResponse<FlightSearchResponse> response = flightService.searchFlights(request, pageable);
+
+        // Assert
+        assertThat(response.content()).hasSize(1);
+        assertThat(response.content().get(0)).isSameAs(mappedResponse);
+
+        assertThat(response.page()).isEqualTo(pageable.getPageNumber());
+        assertThat(response.size()).isEqualTo(pageable.getPageSize());
+        assertThat(response.totalElements()).isEqualTo(totalElements);
+        assertThat(response.first()).isTrue();
+        assertThat(response.last()).isTrue();
+
+        verify(flightMapper).toSearchResponse(projection);
+        verify(flightRepository).searchFlights(
+                eq(FROM_CODE),
+                eq(TO_CODE),
+                any(Instant.class),
+                any(Instant.class),
+                eq(FlightStatus.SCHEDULED),
+                eq(pageable)
+        );
+
+    }
+
+    @Test
+    void searchFlights_shouldReturnEmptyPageResponse_whenNoFlightsFound() {
+        // Arrange
+        FlightSearchRequest request = buildSearchRequest();
+        Pageable pageable = PageRequest.of(0, 10);
+        Airport departureAirport = buildDepartureAirport();
+
+        when(airportRepository.findByCode(FROM_CODE))
+                .thenReturn(Optional.of(departureAirport));
+        when(flightRepository.searchFlights(any(), any(), any(), any(), any(), any()))
+                .thenReturn(new PageImpl<>(List.of(), pageable, 0L));
+
+        // Act
+        PageResponse<FlightSearchResponse> response = flightService.searchFlights(request, pageable);
+
+        // Assert
+        assertThat(response.content()).isEmpty();
+        assertThat(response.page()).isEqualTo(pageable.getPageNumber());
+        assertThat(response.size()).isEqualTo(pageable.getPageSize());
+        assertThat(response.totalElements()).isEqualTo(0L);
+        assertThat(response.first()).isTrue();
+        assertThat(response.last()).isTrue();
+    }
+
+    @Test
+    void searchFlights_shouldConvertLocalDateToCorrectUtcRange_whenAirportTimezoneIsKyiv() {
+        // Arrange
+        Airport departureAirport = buildDepartureAirport("Europe/Kyiv");
+        FlightSearchRequest request = buildSearchRequest();
+        Pageable pageable = PageRequest.of(0, 10);
+
+        when(airportRepository.findByCode(FROM_CODE))
+                .thenReturn(Optional.of(departureAirport));
+
+        when(flightRepository.searchFlights(any(), any(), any(), any(), any(), any()))
+                .thenReturn(Page.empty());
+
+        // Act
+        flightService.searchFlights(request, pageable);
+
+        // Assert
+        ArgumentCaptor<Instant> startCaptor = ArgumentCaptor.forClass(Instant.class);
+        ArgumentCaptor<Instant> endCaptor = ArgumentCaptor.forClass(Instant.class);
+
+        verify(flightRepository).searchFlights(
+                eq(FROM_CODE),
+                eq(TO_CODE),
+                startCaptor.capture(),
+                endCaptor.capture(),
+                eq(FlightStatus.SCHEDULED),
+                eq(pageable)
+        );
+
+        ZoneId kyiv = ZoneId.of(departureAirport.getTimeZone());
+
+        Instant expectedStart = SEARCH_DATE.atStartOfDay(kyiv).toInstant();
+        Instant expectedEnd = SEARCH_DATE.plusDays(1).atStartOfDay(kyiv).toInstant();
+
+        assertThat(startCaptor.getValue()).isEqualTo(expectedStart);
+        assertThat(endCaptor.getValue()).isEqualTo(expectedEnd);
+    }
+
+    @Test
+    void searchFlights_shouldConvertLocalDateToCorrectUtcRange_whenAirportTimezoneIsUtc() {
+        // Arrange
+        Airport departureAirport = buildDepartureAirport("UTC");
+        FlightSearchRequest request = buildSearchRequest();
+        Pageable pageable = PageRequest.of(0, 10);
+
+        when(airportRepository.findByCode(FROM_CODE))
+                .thenReturn(Optional.of(departureAirport));
+
+        when(flightRepository.searchFlights(any(), any(), any(), any(), any(), any()))
+                .thenReturn(Page.empty());
+
+        // Act
+        flightService.searchFlights(request, pageable);
+
+        // Assert
+        ArgumentCaptor<Instant> startCaptor = ArgumentCaptor.forClass(Instant.class);
+        ArgumentCaptor<Instant> endCaptor = ArgumentCaptor.forClass(Instant.class);
+
+        verify(flightRepository).searchFlights(
+                eq(FROM_CODE),
+                eq(TO_CODE),
+                startCaptor.capture(),
+                endCaptor.capture(),
+                eq(FlightStatus.SCHEDULED),
+                eq(pageable)
+        );
+
+        ZoneId Utc = ZoneId.of(departureAirport.getTimeZone());
+
+        Instant expectedStart = SEARCH_DATE.atStartOfDay(Utc).toInstant();
+        Instant expectedEnd = SEARCH_DATE.plusDays(1).atStartOfDay(Utc).toInstant();
+
+        assertThat(startCaptor.getValue()).isEqualTo(expectedStart);
+        assertThat(endCaptor.getValue()).isEqualTo(expectedEnd);
+    }
+
+    @Test
+    void searchFlights_shouldThrowEntityNotFoundApiException_whenDepartureAirportNotFound() {
+        // Arrange
+        FlightSearchRequest request = buildSearchRequest();
+        when(airportRepository.findByCode(FROM_CODE)).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThatThrownBy(() -> flightService.searchFlights(request, PageRequest.of(0, 10)))
+                .isInstanceOf(EntityNotFoundApiException.class)
+                .hasMessageContaining(FROM_CODE);
+
+        verifyNoInteractions(flightRepository, flightMapper);
+
+    }
+
+    // -------------------
+    // getFlightDetails
+    // -------------------
+
+    @Test
+    void getFlightDetails_shouldReturnFlightResponse_whenFlightFound() {
+        // Arrange
+        long id = 1L;
+        FlightProjection projection = mock(FlightProjection.class);
+        FlightSearchResponse mappedResponse = buildFlightSearchResponse();
+
+        when(flightRepository.findProjectionById(id))
+                .thenReturn(Optional.of(projection));
+        when(flightMapper.toSearchResponse(projection))
+                .thenReturn(mappedResponse);
+
+        // Act
+        FlightSearchResponse response = flightService.getFlightDetails(id);
+
+        // Assert
+        assertThat(response).isSameAs(mappedResponse);
+
+        verify(flightMapper).toSearchResponse(projection);
+
+    }
+
+    @Test
+    void getFlightDetails_shouldThrowEntityNotFoundApiException_whenFlightNotFound() {
+        // Arrange
+        long id = 1L;
+        when(flightRepository.findProjectionById(id)).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThatThrownBy(() -> flightService.getFlightDetails(id))
+                .isInstanceOf(EntityNotFoundApiException.class)
+                .hasMessageContaining(String.valueOf(id));
+
+        verifyNoInteractions(flightMapper);
+    }
+
+
+    private Airport buildDepartureAirport() {
+        return new Airport(
+                FROM_CODE, "Boryspil International Airport", "Kyiv", "Ukraine", "Europe/Kyiv"
+        );
+    }
+
+    private Airport buildDepartureAirport(String timezone) {
+        return new Airport(
+                FROM_CODE, "Boryspil International Airport", "Kyiv", "Ukraine", timezone
+        );
+    }
+
+    private FlightSearchRequest buildSearchRequest() {
+        return new FlightSearchRequest(FROM_CODE, TO_CODE, SEARCH_DATE);
+    }
+
+    private FlightSearchResponse buildFlightSearchResponse() {
+        return new FlightSearchResponse(
+                1L, "PS101",
+                "KBP", "Kyiv",
+                "LWO", "Lviv",
+                LocalDateTime.of(2026, 10, 20, 8, 0),
+                LocalDateTime.of(2026, 10, 20, 9, 15),
+                "Boeing 737-800", 189,
+                new BigDecimal("1500.00"), "SCHEDULED", 75L
+        );
+    }
+
+}
