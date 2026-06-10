e3ab8ce11e71c438fe703064f96e45a77b0dfe03
Rostislav <rstk.tischenko@gmail.com>
2026-06-10 21:40:57 +0300

    docs: add OpenAPI annotations for BookingController createBooking method

--- src/main/java/io/github/rosstxix/flightbooking/feature/booking/controller/BookingController.java
+++ src/main/java/io/github/rosstxix/flightbooking/feature/booking/controller/BookingController.java
@@ -2,6 +2,16 @@ package io.github.rosstxix.flightbooking.feature.booking.controller;
 
 import io.github.rosstxix.flightbooking.feature.booking.dto.request.BookingCreateRequest;
 import io.github.rosstxix.flightbooking.feature.booking.usecase.CreateBookingUseCase;
+import io.github.rosstxix.flightbooking.infrastructure.error.model.ApiErrorCode;
+import io.github.rosstxix.flightbooking.infrastructure.error.model.ErrorResponse;
+import io.github.rosstxix.flightbooking.infrastructure.openapi.ErrorApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +24,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Validated
+@Tag(name = "Bookings", description = "Booking management")
+@SecurityRequirement(name = "bearerAuth")
 @RestController
 @RequestMapping("/api/bookings")
 public class BookingController {
@@ -24,6 +36,63 @@ public class BookingController {
         this.createBookingUseCase = createBookingUseCase;
     }
 
+    @Operation(
+            summary = "Booking creation",
+            description = "Allows you to book a seat for flight"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201", description = "Booking created successfully"
+            ),
+            @ApiResponse(
+                    responseCode = "400", description = "Invalid request body",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "401", description = "Token is missing or invalid",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "403", description = "Access denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404", description = "Flight you trying to book for was not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "409", description = "Seat you trying to book already booked",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "422", description = "Seat you trying to book does not exist",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )
+            )
+    })
+    @ErrorApiResponse(status = HttpStatus.BAD_REQUEST, errorCode = ApiErrorCode.VALIDATION_ERROR, message = "seatNumber : Seat number must be in the format '1A', '12B', etc")
+    @ErrorApiResponse(status = HttpStatus.UNAUTHORIZED, errorCode = ApiErrorCode.TOKEN_INVALID, message = "JWT token is invalid")
+    @ErrorApiResponse(status = HttpStatus.FORBIDDEN, errorCode = ApiErrorCode.ACCESS_DENIED, message = "Access denied")
+    @ErrorApiResponse(status = HttpStatus.NOT_FOUND, errorCode = ApiErrorCode.ENTITY_NOT_FOUND, message = "Flight with id 1000 was not found")
+    @ErrorApiResponse(status = HttpStatus.CONFLICT, errorCode = ApiErrorCode.SEAT_ALREADY_BOOKED, message = "Seat with number 1A already booked")
+    @ErrorApiResponse(status = HttpStatus.UNPROCESSABLE_ENTITY, errorCode = ApiErrorCode.SEAT_DOES_NOT_EXIST, message = "Seat with number 99R does not exist")
     @PostMapping("/create")
     public ResponseEntity<Void> createBooking(
             @AuthenticationPrincipal Jwt jwt,
